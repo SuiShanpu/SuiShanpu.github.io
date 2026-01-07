ba62efe34b9f02d6a324fe9f05703b1b601100e5
Shanpu <qian5z@163.com>
2026-01-07 17:47:53 +0800

    feat: 首页面使用路由展示。

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<!--
+  创建时间: 2026-01-07 17:37:25
+  作者: Shanpu
+  功能: Home
+-->
+
+<script setup>
+
+</script> 
+
+<template>
+  <div class="">
+    <h1 class="green">你好</h1>
+    <h3>
+      欢迎来到山璞的主页
+    </h3>
+  </div>
+</template>
+
+<style lang='less' scoped>
+</style>
